724afe0c1d6d9dfa4f2d3605016c0a78df0a0f73
TrueFocus <joostkrijnbuursink@gmail.com>
2024-04-21 01:33:31 +0200

    Added combination of standard dice feature

--- D_infinity/D_infinity.ino
+++ D_infinity/D_infinity.ino
@@ -3,11 +3,13 @@
 
 LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
+// Variables determining pins connected to the different buttons
 const int selButton = 5;
 const int leftButton = 4;
 const int rightButton = 3;
 const int acceptButton = 2;
 
+// Variables for button states
 int currentButtonStateSel = 0; 
 int lastButtonStateSel = 0; 
 int currentButtonStateL = 0; 
@@ -17,41 +19,52 @@ int lastButtonStateR = 0;
 int currentButtonStateA = 0; 
 int lastButtonStateA = 0; 
 
+// Variables for menuing
 int menuValue = 0;
 int subMenuValue = 0;
 int choiceValue = 0;
-int diceValue = 20;
 bool inResultMenu = false;
+bool inSubMenu = false;
+
+// Values for dice rolls and results
+int diceValue = 20;
 long diceResult = 0;
+
+// Variables for creating the proper display
 String result = "0000"; 
 String leadingZeroes = "0";
-bool inSubMenu = false;
 String displayValue = " d20";
 
 void setup()
 {
-  lcd.init();                      // initialize the lcd 
+  // Initialize lcd, serial monitor and pins
+  lcd.init();                      
   lcd.backlight();
   Serial.begin(9600);
   pinMode(selButton, INPUT);
   pinMode(leftButton, INPUT);
   pinMode(rightButton, INPUT);
   pinMode(acceptButton, INPUT);
+
+  // Generate random seed based on error readings from analog pin 0
   randomSeed(analogRead(0));
 }
 
 void loop()
 {
+  // Save previous button values to register one button press on hold of the button
   lastButtonStateSel = currentButtonStateSel;
   lastButtonStateL = currentButtonStateL;
   lastButtonStateR = currentButtonStateR;
   lastButtonStateA = currentButtonStateA;
+
+   // Register current input from the 4 buttons
   currentButtonStateSel = digitalRead(selButton);
   currentButtonStateL = digitalRead(leftButton);
   currentButtonStateR = digitalRead(rightButton);
   currentButtonStateA = digitalRead(acceptButton);
   
-  // switch through menus
+  // Switch through main menus using the select button
   if (currentButtonStateSel == HIGH && lastButtonStateSel == LOW) {        
         menuValue++;
         if(menuValue >= 3){
@@ -60,211 +73,36 @@ void loop()
         choiceValue = 0;
         subMenuValue = 0;
         inResultMenu = false;
+        result = "0000";
+        diceResult = 0;
   } 
 
-  // debug that shows current menu
-
-
-switch (menuValue) {
-case 0:
-    if(inResultMenu == false){
-      if (currentButtonStateR == HIGH && lastButtonStateR == LOW) {        
-        choiceValue++;
-        if(choiceValue >= 8){
-          choiceValue = 0;
-        }
-      } else if(currentButtonStateL == HIGH && lastButtonStateL == LOW){
-        choiceValue--;
-        if(choiceValue <= 0){
-          choiceValue = 7;
-        }
-      }
-
-      switch(choiceValue){
-       case 0:
-          displayValue = " d20"; 
-          diceValue = 20;
-          break;
-       case 1:
-          displayValue = "  d2"; 
-          diceValue = 2; 
-          break;
-       case 2:
-          displayValue = "  d4"; 
-          diceValue = 4; 
-          break;
-       case 3:
-          displayValue = "  d6"; 
-          diceValue = 6; 
-          break;
-       case 4:
-          displayValue = "  d8"; 
-          diceValue = 8; 
-          break;
-       case 5:
-          displayValue = " d10"; 
-          diceValue = 10; 
-          break;
-       case 6:
-          displayValue = " d12"; 
-          diceValue = 12; 
-          break;
-       case 7:
-          displayValue = "d100"; 
-          diceValue = 100; 
-          break;
-      }
-    } else if (inResultMenu){
-       displayValue = result;
+  // If we're not displaying a roll result perform the functions of each different menu to combine dice and modifiers
+  if(inResultMenu){
+      displayValue = result;
+    if(currentButtonStateA == HIGH && lastButtonStateA == LOW){
+      inResultMenu = false;
+      result = "0000";
     }
-    
-      if (currentButtonStateA == HIGH && lastButtonStateA == LOW && inResultMenu == false) {        
-          
-          inResultMenu = true;
-          diceResult = random(1, diceValue+1);
-  
-          if(diceResult<10){
-            leadingZeroes = "   ";
-            result = leadingZeroes + diceResult;
-          }else if(diceResult<100){
-            leadingZeroes = "  ";
-            result = leadingZeroes + diceResult;
-          }else if(diceResult<=1000){
-            leadingZeroes = " ";
-            result = leadingZeroes + diceResult;
-          }else{
-            result = String(diceResult);
-          }
-          
-          Serial.println(diceResult);
-          Serial.println(result);
-          
-      } else if(currentButtonStateA == HIGH && lastButtonStateA == LOW && inResultMenu == true){
-        
-          inResultMenu = false;
-          result = "0000";
-          
-      }
-      
+  }else{
+    switch (menuValue) {
+    case 0:
+      singleDie();
       break;
-      
-case 1:
-      if (currentButtonStateA == HIGH && lastButtonStateA == LOW) {    
-          choiceValue = 0;
-          if(subMenuValue == 1 && choiceValue == 9){
-            inResultMenu = true;
-            subMenuValue = 0;
-          } else{
-            if(subMenuValue<2){
-               subMenuValue++;
-            }else{
-              subMenuValue = 0;
-            }
-            inResultMenu = false;
-          }
-      }
-
-      if(inResultMenu){
-        displayValue = result;
-        break;
-      }
-      
-      switch(subMenuValue){
-        case 0:
-        
-          displayValue = "COMB";
-        
-        break;
-        case 1:
-        
-        if(!inResultMenu){
-          if (currentButtonStateR == HIGH && lastButtonStateR == LOW) 
-          {        
-              choiceValue++;
-              if(choiceValue >= 10)
-              {
-                choiceValue = 0;
-              }
-          } else if(currentButtonStateL == HIGH && lastButtonStateL == LOW)
-          {
-            choiceValue--;
-            if(choiceValue < 0)
-            {
-              choiceValue = 9;
-            }
-  
-           switch(choiceValue)
-           {
-               case 0:
-                  displayValue = " d20"; 
-                  diceValue = 20;
-                  break;
-               case 1:
-                  displayValue = "  d2"; 
-                  diceValue = 2; 
-                  break;
-               case 2:
-                  displayValue = "  d4"; 
-                  diceValue = 4; 
-                  break;
-               case 3:
-                  displayValue = "  d6"; 
-                  diceValue = 6; 
-                  break;
-               case 4:
-                  displayValue = "  d8"; 
-                  diceValue = 8; 
-                  break;
-               case 5:
-                  displayValue = " d10"; 
-                  diceValue = 10; 
-                  break;
-               case 6:
-                  displayValue = " d12"; 
-                  diceValue = 12; 
-                  break;
-               case 7:
-                  displayValue = "d100"; 
-                  diceValue = 100; 
-                  break;
-               case 8:
-                 displayValue = " MOD";
-                 diceValue = 1; 
-                 break;
-               case 9:
-                 displayValue = "ROLL";
-                 break; 
-            }
-          } 
-        }   
-        break;
-        case 2:
-//          if (currentButtonStateR == HIGH && lastButtonStateR == LOW) 
-//          {        
-//              choiceValue++;
-//              if(choiceValue >= 10)
-//              {
-//                choiceValue = 0;
-//              }
-//          } else if(currentButtonStateL == HIGH && lastButtonStateL == LOW)
-//          {
-//            choiceValue--;
-//            if(choiceValue < 0)
-//            {
-//              choiceValue = 7;
-//            }
-//          }
-        break;
-      }
+    case 1:
+      combinationDice();
+      break;  
+    case 2:
+      customDice();
       break;
-case 2:
-      displayValue = "CUST";
-      break;
+    }
   }
 
+  // Render graphical interface 
   lcd.clear();
-  lcd.setCursor(0,0);
   lcd.print(displayValue);
+
+  // All of these extra lcd prints are just for debugging and completely unnecessary
   lcd.setCursor(0,1);
   lcd.print("M");
   lcd.setCursor(1,1);
@@ -286,8 +124,165 @@ case 2:
     lcd.print("n");
   }
 
-    delay(100);
+  // Delay to reduce refresh rate of the screen in a janky way -> should use custom delay that doesn't interfere with any processes. Counter system?
+  delay(50);
+}
 
- 
-  
+void singleDie(){
+  // Use left and right buttons to determine a dice choice.
+  if (currentButtonStateR == HIGH && lastButtonStateR == LOW) {        
+    choiceValue++;
+    choiceValue = choiceValue >= 8 ? 0 : choiceValue;
+  } else if(currentButtonStateL == HIGH && lastButtonStateL == LOW){
+    choiceValue--;
+    choiceValue = choiceValue < 0 ? 7 : choiceValue;
+  }
+
+  // Choose die and display properties based on the choice value
+  chooseDie();
+
+  // Go to result menu and determine what to display there.
+  if (currentButtonStateA == HIGH && lastButtonStateA == LOW && inResultMenu == false) {        
+      inResultMenu = true;
+      diceResult = random(1, diceValue+1);
+      if(diceResult<10){
+        leadingZeroes = "   ";
+        result = leadingZeroes + diceResult;
+      }else if(diceResult<100){
+        leadingZeroes = "  ";
+        result = leadingZeroes + diceResult;
+      }else if(diceResult<=1000){
+        leadingZeroes = " ";
+        result = leadingZeroes + diceResult;
+      }else{
+        result = String(diceResult);
+      }
+  }
+}
+
+void combinationDice(){
+  switch (subMenuValue){
+    case 0:
+      if(currentButtonStateA == HIGH && lastButtonStateA == LOW){    
+        subMenuValue++;
+      }else{
+        displayValue = "COMB";
+      }
+      break;
+    case 1:
+      // Use left and right buttons to determine a dice choice.
+      if (currentButtonStateR == HIGH && lastButtonStateR == LOW) {        
+        choiceValue++;
+        choiceValue = choiceValue >= 10 ? 0 : choiceValue;
+      } else if(currentButtonStateL == HIGH && lastButtonStateL == LOW){
+        choiceValue--;
+        choiceValue = choiceValue < 0 ? 9 : choiceValue;
+      }
+
+      // Choose die and display properties based on the choice value
+      if(choiceValue==8){
+        displayValue = " MOD";
+        diceValue = 1; 
+      }else if(choiceValue==9){
+        displayValue = "ROLL";
+      }else{
+        chooseDie();
+      }
+
+      // Go to result menu if ROLL is selected, otherwise choose number of dice to add to result.
+      if(currentButtonStateA == HIGH && lastButtonStateA == LOW && choiceValue == 9){    
+        inResultMenu = true;
+        if(diceResult<10){
+          leadingZeroes = "   ";
+          result = leadingZeroes + diceResult;
+        }else if(diceResult<100){
+          leadingZeroes = "  ";
+          result = leadingZeroes + diceResult;
+        }else if(diceResult<=1000){
+          leadingZeroes = " ";
+          result = leadingZeroes + diceResult;
+        }else{
+          result = String(diceResult);
+        }
+      }else if(currentButtonStateA == HIGH && lastButtonStateA == LOW){
+        choiceValue = 1;
+        subMenuValue++;
+      }
+      break;
+    case 2:
+      // Choose the number of dice to add to the total
+      if (currentButtonStateR == HIGH && lastButtonStateR == LOW) {        
+        choiceValue++;
+        choiceValue = choiceValue >= 101 ? 0 : choiceValue;
+      } else if(currentButtonStateL == HIGH && lastButtonStateL == LOW){
+        choiceValue--;
+        choiceValue = choiceValue < 0 ? 100 : choiceValue;
+      }
+
+      // Add leading zeros to the display
+      if(choiceValue<10){
+        leadingZeroes = "   ";
+        displayValue = leadingZeroes + choiceValue;
+      }else if(choiceValue<100){
+        leadingZeroes = "  ";
+        displayValue = leadingZeroes + choiceValue;
+      }else if(choiceValue<=1000){
+        leadingZeroes = " ";
+        displayValue = leadingZeroes + choiceValue;
+      }else{
+        displayValue = String(choiceValue);
+      }
+
+      // If the accept button is pressed add the chosen die choiceValue number of times and add it to the diceresult
+      if(currentButtonStateA == HIGH && lastButtonStateA == LOW){    
+        for (int i = 1; i <= choiceValue; i++) {
+          diceResult = diceResult + random(1, diceValue+1); 
+          Serial.println(diceResult);
+        }
+        subMenuValue = 1;
+      }
+      break;
+  }
+}
+
+void customDice(){
+  displayValue = "CUST";
+}
+
+void chooseDie(){
+  switch(choiceValue)
+    {
+      case 0:
+        displayValue = " d20"; 
+        diceValue = 20;
+        break;
+      case 1:
+        displayValue = "  d2"; 
+        diceValue = 2; 
+        break;
+      case 2:
+        displayValue = "  d4"; 
+        diceValue = 4; 
+        break;
+      case 3:
+        displayValue = "  d6"; 
+        diceValue = 6; 
+        break;
+      case 4:
+        displayValue = "  d8"; 
+        diceValue = 8; 
+        break;
+      case 5:
+        displayValue = " d10"; 
+        diceValue = 10; 
+        break;
+      case 6:
+        displayValue = " d12"; 
+        diceValue = 12; 
+        break;
+      case 7:
+        displayValue = "d100"; 
+        diceValue = 100; 
+        break;
+    }
 }
